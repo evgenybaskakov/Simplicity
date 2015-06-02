5c9a250f3ec1ea62e8f0f37955abf44e104f135e
Evgeny Baskakov <evgenybaskakov@evgenys-mbp.attlocal.net>
2015-06-01 22:51:40 -0700

    New operation: SMOpSendMessage

---
+++ Simplicity/SMOpSendMessage.h
@@ -0,0 +1,17 @@
+//
+//  SMOpSendMessage.h
+//  Simplicity
+//
+//  Created by Evgeny Baskakov on 6/1/15.
+//  Copyright (c) 2015 Evgeny Baskakov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "SMOperation.h"
+
+@interface SMOpSendMessage : SMOperation
+
+- (id)initWithMessage:(MCOMessageBuilder*)message;
+
+@end
---
+++ Simplicity/SMOpSendMessage.m
@@ -0,0 +1,59 @@
+//
+//  SMOpSendMessage.m
+//  Simplicity
+//
+//  Created by Evgeny Baskakov on 6/1/15.
+//  Copyright (c) 2015 Evgeny Baskakov. All rights reserved.
+//
+
+#import <MailCore/MailCore.h>
+
+#import "SMAppDelegate.h"
+#import "SMOpSendMessage.h"
+
+@implementation SMOpSendMessage {
+    MCOMessageBuilder *_message;
+    MCOSMTPOperation *_currentOp;
+}
+
+- (id)initWithMessage:(MCOMessageBuilder*)message {
+    self = [super init];
+    
+    if(self) {
+        _message = message;
+    }
+    
+    return self;
+}
+
+- (void)start {
+    [self startInternal];
+}
+
+- (void)cancel {
+    [self cancelInternal];
+}
+
+- (void)startInternal {
+    SMAppDelegate *appDelegate = [[NSApplication sharedApplication] delegate];
+    MCOSMTPOperation *op = [[[appDelegate model] smtpSession] sendOperationWithData:_message.data];
+    
+    _currentOp = op;
+    
+    [op start:^(NSError * error) {
+        if (error != nil && [error code] != MCOErrorNone) {
+            NSLog(@"%s: Error sending message: %@", __func__, error);
+            
+            [self startInternal]; // TODO!!
+        }
+        
+        NSLog(@"%s: message sent successfully", __func__);
+    }];
+}
+
+- (void)cancelInternal {
+    [_currentOp cancel];
+    _currentOp = nil;
+}
+
+@end

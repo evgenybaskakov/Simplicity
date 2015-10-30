c2308a0656762e7fc7ceb208c51b63d2e877f365
Evgeny Baskakov <evgeny.baskakov@gmail.com>
2015-10-29 23:27:18 -0700

    Preferences, episode 1: Account initial design sketched

---
+++ Simplicity/SMAccountPreferencesViewController.h
@@ -0,0 +1,13 @@
+//
+//  SMAccountPreferencesViewController.h
+//  Simplicity
+//
+//  Created by Evgeny Baskakov on 10/29/15.
+//  Copyright © 2015 Evgeny Baskakov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SMAccountPreferencesViewController : NSViewController
+
+@end
---
+++ Simplicity/SMAccountPreferencesViewController.m
@@ -0,0 +1,22 @@
+//
+//  SMAccountPreferencesViewController.m
+//  Simplicity
+//
+//  Created by Evgeny Baskakov on 10/29/15.
+//  Copyright © 2015 Evgeny Baskakov. All rights reserved.
+//
+
+#import "SMAccountPreferencesViewController.h"
+
+@interface SMAccountPreferencesViewController ()
+
+@end
+
+@implementation SMAccountPreferencesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end

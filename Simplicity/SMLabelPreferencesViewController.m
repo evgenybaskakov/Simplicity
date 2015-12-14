105b912b8b141905019c364db7d62b872641a897
Evgeny Baskakov <evgeny.baskakov@gmail.com>
2015-12-13 22:45:06 -0800

    Label preferences: a few additions

--- Simplicity/SMLabelPreferencesViewController.m
+++ Simplicity/SMLabelPreferencesViewController.m
@@ -6,36 +6,60 @@
 //  Copyright © 2015 Evgeny Baskakov. All rights reserved.
 //
 
+#import "SMLog.h"
 #import "SMLabelPreferencesViewController.h"
 
 @interface SMLabelPreferencesViewController ()
+
 @property (weak) IBOutlet NSPopUpButton *accountList;
 @property (weak) IBOutlet NSTableView *labelTable;
 @property (weak) IBOutlet NSButton *addLabelButton;
 @property (weak) IBOutlet NSButton *removeLabelButton;
 @property (weak) IBOutlet NSButton *reloadLabelsButton;
 @property (weak) IBOutlet NSProgressIndicator *reloadProgressIndicator;
+
 @end
 
-@implementation SMLabelPreferencesViewController
+@implementation SMLabelPreferencesViewController {
+    NSUInteger _selectedAccount;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
 
     // Do view setup here.
 
+    _selectedAccount = 0; // TODO: use current account; reset this if account is deleted
+
+    _reloadProgressIndicator.hidden = YES;
 }
 
 - (IBAction)accountListAction:(id)sender {
+    SM_LOG_INFO(@"TODO");
 }
 
 - (IBAction)addLabelAction:(id)sender {
+    SM_LOG_INFO(@"TODO");
 }
 
 - (IBAction)removeLabelAction:(id)sender {
+    SM_LOG_INFO(@"TODO");
 }
 
 - (IBAction)reloadLabelsAction:(id)sender {
+    SM_LOG_INFO(@"TODO");
+}
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView {
+    return 2;
+}
+
+- (IBAction)selectLabelColorAction:(id)sender {
+    SM_LOG_INFO(@"TODO");
+}
+
+- (IBAction)labelVisibleCheckAction:(id)sender {
+    SM_LOG_INFO(@"TODO");
 }
 
 @end

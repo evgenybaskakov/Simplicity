eb2d1713cea2abcb54965cfcc0a9142ee2085ddc
Evgeny Baskakov <evgeny.baskakov@gmail.com>
2015-12-13 22:10:17 -0800

    Label preferences: IB outlets and actions added

--- Simplicity/SMLabelPreferencesViewController.h
+++ Simplicity/SMLabelPreferencesViewController.h
@@ -8,6 +8,6 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface SMLabelPreferencesViewController : NSViewController
+@interface SMLabelPreferencesViewController : NSViewController<NSTableViewDataSource, NSTableViewDelegate>
 
 @end
--- Simplicity/SMLabelPreferencesViewController.m
+++ Simplicity/SMLabelPreferencesViewController.m
@@ -9,14 +9,33 @@
 #import "SMLabelPreferencesViewController.h"
 
 @interface SMLabelPreferencesViewController ()
-
+@property (weak) IBOutlet NSPopUpButton *accountList;
+@property (weak) IBOutlet NSTableView *labelTable;
+@property (weak) IBOutlet NSButton *addLabelButton;
+@property (weak) IBOutlet NSButton *removeLabelButton;
+@property (weak) IBOutlet NSButton *reloadLabelsButton;
+@property (weak) IBOutlet NSProgressIndicator *reloadProgressIndicator;
 @end
 
 @implementation SMLabelPreferencesViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
     // Do view setup here.
+
+}
+
+- (IBAction)accountListAction:(id)sender {
+}
+
+- (IBAction)addLabelAction:(id)sender {
+}
+
+- (IBAction)removeLabelAction:(id)sender {
+}
+
+- (IBAction)reloadLabelsAction:(id)sender {
 }
 
 @end

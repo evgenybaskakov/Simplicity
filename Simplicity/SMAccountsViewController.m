af6479e26983c4ad80a2eb067a888a4aa549db0e
Evgeny Baskakov <evgeny.baskakov@gmail.com>
2016-03-12 22:54:40 -0800

    Account buttons now aligned properly; each contains the respective account info

--- Simplicity/SMAccountsViewController.m
+++ Simplicity/SMAccountsViewController.m
@@ -9,12 +9,14 @@
 #import "SMLog.h"
 #import "SMAppDelegate.h"
 #import "SMAppController.h"
+#import "SMPreferencesController.h"
 #import "SMFlippedView.h"
 #import "SMMailboxViewController.h"
+#import "SMAccountButtonViewController.h"
 #import "SMAccountsViewController.h"
 
 @implementation SMAccountsViewController {
-    NSMutableArray<NSView*> *_accountCells;
+    NSMutableArray<SMAccountButtonViewController*> *_accountButtons;
     NSScrollView *_scrollView;
     NSView *_contentView;
 }
@@ -37,7 +39,7 @@
         
         [_scrollView setDocumentView:_contentView];
         
-        _accountCells = [NSMutableArray new];
+        _accountButtons = [NSMutableArray new];
     }
     
     return self;
@@ -51,25 +53,76 @@
 }
 
 - (void)reloadAccounts {
-    [_accountCells removeAllObjects];
-    
     SMAppDelegate *appDelegate = [[NSApplication sharedApplication] delegate];
     SMAppController *appController = [appDelegate appController];
 
-    NSView *mailboxView = [appController.mailboxViewController view];
-    mailboxView.translatesAutoresizingMaskIntoConstraints = NO;
-    
-    [_accountCells addObject:mailboxView];
+    [_accountButtons removeAllObjects];
 
-    [_contentView addSubview:mailboxView];
+    ///
+    for(NSUInteger i = 0; i < [[appDelegate preferencesController] accountsCount]; i++) {
+        SMAccountButtonViewController *button = [[SMAccountButtonViewController alloc] initWithNibName:nil bundle:nil];
+        NSAssert(button.view, @"button.view");
 
-    [_contentView addConstraint:[NSLayoutConstraint constraintWithItem:_contentView attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:mailboxView attribute:NSLayoutAttributeLeading multiplier:1.0 constant:0]];
+        button.view.translatesAutoresizingMaskIntoConstraints = NO;
+        
+        button.accountIdx = i;
+        
+        button.accountName.stringValue = [[appDelegate preferencesController] accountName:i];
+        if([[[[NSApplication sharedApplication] delegate] preferencesController] shouldShowEmailAddressesInMailboxes]) {
+            button.accountName.stringValue = [[[[NSApplication sharedApplication] delegate] preferencesController] userEmail:i];
+        }
+        else {
+            button.accountName.stringValue = [[[[NSApplication sharedApplication] delegate] preferencesController] accountName:i];
+        }
+        
+        NSString *accountImagePath = [[[[NSApplication sharedApplication] delegate] preferencesController] accountImagePath:i];
+        NSAssert(accountImagePath != nil, @"accountImagePath is nil");
+        
+        button.accountImage.image = [[NSImage alloc] initWithContentsOfFile:accountImagePath];
+        
+        [_accountButtons addObject:button];
+    }
+    ///
+    
+    NSAssert(_accountButtons.count > 0, @"_accountButtons.count == 0");
+    
+    NSView *prevView = nil;
+    for(NSUInteger i = 0; i < _accountButtons.count; i++) {
+        NSView *buttonView = _accountButtons[i].view;
+        
+        [_contentView addSubview:buttonView];
+        
+        [_contentView addConstraint:[NSLayoutConstraint constraintWithItem:_contentView attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:buttonView attribute:NSLayoutAttributeLeading multiplier:1.0 constant:0]];
+        
+        [_contentView addConstraint:[NSLayoutConstraint constraintWithItem:_contentView attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:buttonView attribute:NSLayoutAttributeTrailing multiplier:1.0 constant:0]];
 
-    [_contentView addConstraint:[NSLayoutConstraint constraintWithItem:_contentView attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:mailboxView attribute:NSLayoutAttributeTrailing multiplier:1.0 constant:0]];
+        if(i == 0) {
+            [_contentView addConstraint:[NSLayoutConstraint constraintWithItem:_contentView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:buttonView attribute:NSLayoutAttributeTop multiplier:1.0 constant:0]];
+        }
+        else {
+            [_contentView addConstraint:[NSLayoutConstraint constraintWithItem:prevView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:buttonView attribute:NSLayoutAttributeTop multiplier:1.0 constant:0]];
+        }
+        
+        if(i == appDelegate.currentAccount) {
+            NSView *mailboxView = [appController.mailboxViewController view];
+            mailboxView.translatesAutoresizingMaskIntoConstraints = NO;
+
+            [_contentView addSubview:mailboxView];
+            
+            [_contentView addConstraint:[NSLayoutConstraint constraintWithItem:_contentView attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:mailboxView attribute:NSLayoutAttributeLeading multiplier:1.0 constant:0]];
+            
+            [_contentView addConstraint:[NSLayoutConstraint constraintWithItem:_contentView attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:mailboxView attribute:NSLayoutAttributeTrailing multiplier:1.0 constant:0]];
+            
+            [_contentView addConstraint:[NSLayoutConstraint constraintWithItem:buttonView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:mailboxView attribute:NSLayoutAttributeTop multiplier:1.0 constant:0]];
+            
+            prevView = mailboxView;
+        }
+        else {
+            prevView = buttonView;
+        }
+    }
     
-    [_contentView addConstraint:[NSLayoutConstraint constraintWithItem:_contentView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:mailboxView attribute:NSLayoutAttributeTop multiplier:1.0 constant:0]];
-    
-    [_contentView addConstraint:[NSLayoutConstraint constraintWithItem:_contentView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:mailboxView attribute:NSLayoutAttributeBottom multiplier:1.0 constant:0]];
+    [_contentView addConstraint:[NSLayoutConstraint constraintWithItem:_contentView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:prevView attribute:NSLayoutAttributeBottom multiplier:1.0 constant:0]];
 }
 
 @end

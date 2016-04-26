05bbac82229b929b589eb5e6f36a04ffd486c1af
Evgeny Baskakov <evgeny.baskakov@gmail.com>
2016-04-25 23:49:25 -0700

    New message editor toolbar introduced

---
+++ Simplicity/SMMessageEditorToolbarViewController.h
@@ -0,0 +1,13 @@
+//
+//  SMMessageEditorToolbarViewController.h
+//  Simplicity
+//
+//  Created by Evgeny Baskakov on 4/25/16.
+//  Copyright © 2016 Evgeny Baskakov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SMMessageEditorToolbarViewController : NSViewController
+
+@end
---
+++ Simplicity/SMMessageEditorToolbarViewController.m
@@ -0,0 +1,22 @@
+//
+//  SMMessageEditorToolbarViewController.m
+//  Simplicity
+//
+//  Created by Evgeny Baskakov on 4/25/16.
+//  Copyright © 2016 Evgeny Baskakov. All rights reserved.
+//
+
+#import "SMMessageEditorToolbarViewController.h"
+
+@interface SMMessageEditorToolbarViewController ()
+
+@end
+
+@implementation SMMessageEditorToolbarViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end

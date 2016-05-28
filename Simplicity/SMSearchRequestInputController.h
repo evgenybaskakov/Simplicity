cc595e5edfa0707d62e77fcaf77f98bf1d80612e
Evgeny Baskakov <evgeny.baskakov@gmail.com>
2016-05-28 00:04:48 -0700

    Restructuring; SMSearchRequestInputController added

---
+++ Simplicity/SMSearchRequestInputController.h
@@ -0,0 +1,13 @@
+//
+//  SMSearchRequestInputController.h
+//  Simplicity
+//
+//  Created by Evgeny Baskakov on 5/28/16.
+//  Copyright © 2016 Evgeny Baskakov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SMSearchRequestInputController : NSObject
+
+@end
---
+++ Simplicity/SMSearchRequestInputController.m
@@ -0,0 +1,13 @@
+//
+//  SMSearchRequestInputController.m
+//  Simplicity
+//
+//  Created by Evgeny Baskakov on 5/28/16.
+//  Copyright © 2016 Evgeny Baskakov. All rights reserved.
+//
+
+#import "SMSearchRequestInputController.h"
+
+@implementation SMSearchRequestInputController
+
+@end

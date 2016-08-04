50be830155fb3261216499be48fc4aee9a25f500
Evgeny Baskakov <evgeny.baskakov@gmail.com>
2016-08-03 23:25:58 -0700

    SMLabelWithCloseButton added

---
+++ Simplicity/SMLabelWithCloseButton.h
@@ -0,0 +1,13 @@
+//
+//  SMLabelWithCloseButton.h
+//  Simplicity
+//
+//  Created by Evgeny Baskakov on 8/3/16.
+//  Copyright © 2016 Evgeny Baskakov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SMLabelWithCloseButton : NSViewController
+
+@end
---
+++ Simplicity/SMLabelWithCloseButton.m
@@ -0,0 +1,22 @@
+//
+//  SMLabelWithCloseButton.m
+//  Simplicity
+//
+//  Created by Evgeny Baskakov on 8/3/16.
+//  Copyright © 2016 Evgeny Baskakov. All rights reserved.
+//
+
+#import "SMLabelWithCloseButton.h"
+
+@interface SMLabelWithCloseButton ()
+
+@end
+
+@implementation SMLabelWithCloseButton
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end

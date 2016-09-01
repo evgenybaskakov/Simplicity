e27dec6f5ec729c93b7612bd995f585a91712aa9
Evgeny Baskakov <evgeny.baskakov@gmail.com>
2016-09-01 00:31:50 -0700

    Handling the progress indicator in the image selection dialog

--- Simplicity/SMURLChooserViewController.m
+++ Simplicity/SMURLChooserViewController.m
@@ -18,6 +18,7 @@
 
 @implementation SMURLChooserViewController {
     NSOpenPanel *_fileDlg;
+    NSURLSessionDataTask *_downloadTask;
 }
 
 - (void)viewDidLoad {
@@ -25,11 +26,50 @@
 
     _okButton.enabled = NO;
     _cancelButton.enabled = YES;
-    _progressIndicator.hidden = YES;
+    _progressIndicator.hidden = NO;
+    _progressIndicator.displayedWhenStopped = NO;
+}
+
+- (void)chooseImage:(NSImage*)image url:(NSURL*)url {
+    [_progressIndicator stopAnimation:self];
+
+    _chosenImage = image? image : [NSImage imageNamed:NSImageNameUserGuest];
+    _urlTextField.stringValue = url.absoluteString;
+    _okButton.enabled = YES;
+    
+    if(_target && _actionProbe) {
+        [_target performSelectorOnMainThread:_actionProbe withObject:self waitUntilDone:NO];
+    }
 }
 
 - (IBAction)urlTextFieldAction:(id)sender {
     NSLog(@"urlTextFieldAction");
+
+    [_downloadTask cancel];
+    _downloadTask = nil;
+    
+    NSURL *url = [NSURL URLWithString:_urlTextField.stringValue];
+    if(url == nil) {
+        return;
+    }
+
+    [_progressIndicator startAnimation:self];
+
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    NSURLSession *session = [NSURLSession sharedSession];
+    
+    _downloadTask = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        NSImage *image = nil;
+        if(error == nil && data != nil && [response isKindOfClass:[NSHTTPURLResponse class]] && ((NSHTTPURLResponse*)response).statusCode == 200) {
+            image = [[NSImage alloc] initWithData:data];
+        }
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self chooseImage:image url:url];
+        });
+    }];
+    
+    [_downloadTask resume];
 }
 
 - (IBAction)browseFileButtonAction:(id)sender {
@@ -53,14 +93,7 @@
             }
             
             if(img) {
-                _chosenImage = img;
-                _urlTextField.stringValue = url.absoluteString;
-                _okButton.enabled = YES;
-                
-                if(_target && _actionProbe) {
-                    [_target performSelectorOnMainThread:_actionProbe withObject:self waitUntilDone:NO];
-                }
-
+                [self chooseImage:img url:url];
                 break;
             }
         }
@@ -72,12 +105,16 @@
 }
 
 - (IBAction)okButtonAction:(id)sender {
+    [_progressIndicator stopAnimation:self];
+
     if(_target && _actionOk) {
         [_target performSelectorOnMainThread:_actionOk withObject:self waitUntilDone:NO];
     }
 }
 
 - (IBAction)cancelButtonAction:(id)sender {
+    [_progressIndicator stopAnimation:self];
+
     if(_target && _actionCancel) {
         [_target performSelectorOnMainThread:_actionCancel withObject:self waitUntilDone:NO];
     }

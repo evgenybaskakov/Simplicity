932b4b80468c730ef3517a3fefb20e4c153fd6fb
Evgeny Baskakov <evgeny.baskakov@gmail.com>
2016-10-09 11:33:54 -0700

    "reply" toolbar button now shows a long-click context menu

---
+++ Simplicity/SMButtonWithMenu.h
@@ -0,0 +1,13 @@
+//
+//  SMButtonWithMenu.h
+//  Simplicity
+//
+//  Created by Evgeny Baskakov on 10/8/16.
+//  Copyright © 2016 Evgeny Baskakov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SMButtonWithMenu : NSButton
+
+@end
